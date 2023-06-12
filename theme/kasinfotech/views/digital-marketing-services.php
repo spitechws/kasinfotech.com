e29c6cb80e98583712e960c51f8be14173fde62b
SPSONI <pm@pronero.in>
2023-06-12 18:12:10 +0530

    Update digital-marketing-services.php

--- theme/kasinfotech/views/digital-marketing-services.php
+++ theme/kasinfotech/views/digital-marketing-services.php
@@ -14,64 +14,30 @@
                 <div class="paketler3__on paketler3__on--onyazi">
                     <div class="paketler3__gorsel paketler3__gorsel--1">
                         <div class="iconw"><i class="h-flaticon-039-servers-5"></i></div>
-                        <h3 class="baslik-sol h-yazi-margin-kucuk">IT Support</h3>
+                        <h3 class="baslik-sol h-yazi-margin-kucuk">Quarterly</h3>
                         <p class="services-kutu2--yazi">
-                            These are the management services we provide the IT support and maintenance. </p>
+                            Digital Marketing Services
+                        </p>
                     </div>
                     <div class="paketler3__icerik">
                         <ul>
-                            <li>✓ Phone Support Service</li>
-                            <li>✓ Remote Connection Service</li>
-                            <li>✓ Onsite Support Service</li>
-                            <li>✗ Consultancy Service</li>
-                            <li>✗ Network Security Service</li>
+                            <li>Social Media Marketing</li>
+                            <li>Content Marketing</li>
+                            <li>Search engine optimization</li>
+                            <li>Email Marketing</li>
+                            <li> Google ads Scene</li>
                         </ul>
                     </div>
                 </div>
                 <div class="paketler3__on paketler3__on--arkayazi paketler3__on--arkayazi-1">
                     <div class="paketler3__pr">
                         <div class="paketler3__pr-kutu">
-                            <p class="paketler3__pr-yazi">IT Support</p>
-                            <p class="paketler3__pr-degeri">$1800</p>
+                            <p class="paketler3__pr-yazi">Quarterly</p>
+                            <p class="paketler3__pr-degeri">Rs. 28,000</p>
                         </div>
                         <a href="#popup-it-support" class="custom-buttonw">Send Request!</a>
                     </div>
                 </div>
-            </div>
-            <!-- POPUP FORMS -->
-            <div role="form" class="wpcf7" id="wpcf7-f768-o1" lang="en-US" dir="ltr">
-                <div class="screen-reader-response">
-                    <p role="status" aria-live="polite" aria-atomic="true"></p>
-                    <ul></ul>
-                </div>
-                <form method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">                   
-                    <div class="popup" id="popup-it-support">
-                        <div class="popup__content">
-                            <p><a href="#special-paket-alani" class="popup__kapat">&times;</a></p>
-                            <h2 class="h2-baslik-popup h-yazi-margin-kucuk"> IT Support Pack </h2>
-                            <p class="paragraf-popup">
-                                Application Form
-                            </p>
-                            <div class="form-popup">
-                                <div class="form-popup__grup">
-                                    <span class="wpcf7-form-control-wrap" data-name="text-667"><input type="text" name="text-667" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Full Name"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <span class="wpcf7-form-control-wrap" data-name="email-217"><input type="email" name="email-217" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-popup__input" aria-required="true" aria-invalid="false" placeholder="Email Address"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <span class="wpcf7-form-control-wrap" data-name="text-661"><input type="text" name="text-661" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Phone Number"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <div class="or">
-                                        <input type="submit" value="Submit Form →" class="wpcf7-form-control has-spinner wpcf7-submit custom-button12">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="wpcf7-response-output" aria-hidden="true"></div>
-                </form>
             </div>
         </div>
         <div class="tablo--1-ve-3">
@@ -79,131 +45,63 @@
                 <div class="paketler3__on paketler3__on--onyazi">
                     <div class="paketler3__gorsel paketler3__gorsel--1">
                         <div class="iconw"><i class="h-flaticon-042-servers-4"></i></div>
-                        <h3 class="baslik-sol h-yazi-margin-kucuk"> Cloud Service</h3>
+                        <h3 class="baslik-sol h-yazi-margin-kucuk">Half Yearly</h3>
                         <p class="services-kutu2--yazi wow fade">
-                            It is the process of configuring virtual cloud systems that businesses need. </p>
+                            Digital Marketing Services
+                        </p>
                     </div>
                     <div class="paketler3__icerik">
                         <ul>
-                            <li>✓ Phone Support Service</li>
-                            <li>✓ Remote Connection Service</li>
-                            <li>✓ Onsite Support Service</li>
-                            <li>✓ Consultancy Service</li>
-                            <li>✗ Network Security Service</li>
+                            <li>Social Media Marketing</li>
+                            <li>Content Marketing</li>
+                            <li>Search engine optimization</li>
+                            <li>Email Marketing</li>
+                            <li> Google ads Scene</li>
                         </ul>
                     </div>
                 </div>
                 <div class="paketler3__on paketler3__on--arkayazi paketler3__on--arkayazi-1">
                     <div class="paketler3__pr">
                         <div class="paketler3__pr-kutu">
-                            <p class="paketler3__pr-yazi">Cloud Service</p>
-                            <p class="paketler3__pr-degeri">$2500</p>
+                            <p class="paketler3__pr-yazi">Half Yearly</p>
+                            <p class="paketler3__pr-degeri">Rs.40,000</p>
                         </div>
                         <a href="#popup-cloud-service" class="custom-buttonw">Send Request!</a>
                     </div>
                 </div>
             </div>
-            <!-- POPUP FORMS -->
-            <div role="form" class="wpcf7" id="wpcf7-f770-o2" lang="en-US" dir="ltr">
-                <div class="screen-reader-response">
-                    <p role="status" aria-live="polite" aria-atomic="true"></p>
-                    <ul></ul>
-                </div>
-                <form action="/wordpress/datax/#wpcf7-f770-o2" method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
-                   
-                    <div class="popup" id="popup-cloud-service">
-                        <div class="popup__content">
-                            <p><a href="#special-paket-alani" class="popup__kapat">&times;</a></p>
-                            <h2 class="h2-baslik-popup h-yazi-margin-kucuk"> Cloud Service Pack </h2>
-                            <p class="paragraf-popup">
-                                Application Form
-                            </p>
-                            <div class="form-popup">
-                                <div class="form-popup__grup">
-                                    <span class="wpcf7-form-control-wrap" data-name="text-667"><input type="text" name="text-667" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Full Name"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <span class="wpcf7-form-control-wrap" data-name="email-217"><input type="email" name="email-217" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-popup__input" aria-required="true" aria-invalid="false" placeholder="Email Address"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <span class="wpcf7-form-control-wrap" data-name="text-661"><input type="text" name="text-661" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Phone Number"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <div class="or">
-                                        <input type="submit" value="Submit Form →" class="wpcf7-form-control has-spinner wpcf7-submit custom-button12">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="wpcf7-response-output" aria-hidden="true"></div>
-                </form>
-            </div>
+
         </div>
         <div class="tablo--1-ve-3">
             <div class="paketler3 wow flipInY" data-wow-delay="0.8s" data-tilt="">
                 <div class="paketler3__on paketler3__on--onyazi">
                     <div class="paketler3__gorsel paketler3__gorsel--1">
                         <div class="iconw"><i class="h-flaticon-040-server"></i></div>
-                        <h3 class="baslik-sol h-yazi-margin-kucuk">Server Virtualization</h3>
+                        <h3 class="baslik-sol h-yazi-margin-kucuk">Yearly</h3>
                         <p class="services-kutu2--yazi wow fade">
-                            It is the process of configuring virtual servers that businesses need. </p>
+                            Digital Marketing Services</p>
                     </div>
                     <div class="paketler3__icerik">
                         <ul>
-                            <li>✓ Phone Support Service</li>
-                            <li>✓ Remote Connection Service</li>
-                            <li>✓ Onsite Support Service</li>
-                            <li>✓ Consultancy Service</li>
-                            <li>✓ Network Security Service</li>
+                            <li>Social Media Marketing</li>
+                            <li>Content Marketing</li>
+                            <li>Search engine optimization</li>
+                            <li>Email Marketing</li>
+                            <li> Google ads Scene</li>
                         </ul>
                     </div>
                 </div>
                 <div class="paketler3__on paketler3__on--arkayazi paketler3__on--arkayazi-1">
                     <div class="paketler3__pr">
                         <div class="paketler3__pr-kutu">
-                            <p class="paketler3__pr-yazi">Server Virtualization</p>
-                            <p class="paketler3__pr-degeri">$5000</p>
+                            <p class="paketler3__pr-yazi">Yearly</p>
+                            <p class="paketler3__pr-degeri">Rs. 60,000</p>
                         </div>
                         <a href="#popup-server-virtual" class="custom-buttonw">Send Request!</a>
                     </div>
                 </div>
             </div>
-            <!-- POPUP FORMS -->
-            <div role="form" class="wpcf7" id="wpcf7-f772-o3" lang="en-US" dir="ltr">
-                <div class="screen-reader-response">
-                    <p role="status" aria-live="polite" aria-atomic="true"></p>
-                    <ul></ul>
-                </div>
-                <form method="post" class="wpcf7-form init" novalidate="novalidate" data-status="init">
-                    <div class="popup" id="popup-server-virtual">
-                        <div class="popup__content">
-                            <p><a href="#special-paket-alani" class="popup__kapat">&times;</a></p>
-                            <h2 class="h2-baslik-popup h-yazi-margin-kucuk"> Server Virtualization Pack </h2>
-                            <p class="paragraf-popup">
-                                Application Form
-                            </p>
-                            <div class="form-popup">
-                                <div class="form-popup__grup">
-                                    <span><input type="text" name="text-667" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Full Name"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <span><input type="email" name="email-217" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form-popup__input" aria-required="true" aria-invalid="false" placeholder="Email Address"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <span><input type="text" name="text-661" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form-popup__input" aria-required="true" aria-invalid="false" placeholder="Phone Number"></span>
-                                </div>
-                                <div class="form-popup__grup">
-                                    <div class="or">
-                                        <input type="submit" value="Submit Form →" class="wpcf7-form-control has-spinner wpcf7-submit custom-button12">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="wpcf7-response-output" aria-hidden="true"></div>
-                </form>
-            </div>
+
         </div>
     </div>
 </section>
